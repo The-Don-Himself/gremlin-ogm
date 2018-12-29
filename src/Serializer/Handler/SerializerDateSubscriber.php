b9f6dd09897ffcf87db8d5a3846e653e36afe95c
The-Don_Himself <don.e.omondi@gmail.com>
2018-12-29 15:53:47 +0300

    Simplify DateTime JMS Serializer Subscriber

--- src/Serializer/Handler/SerializerDateSubscriber.php
+++ src/Serializer/Handler/SerializerDateSubscriber.php
@@ -3,7 +3,6 @@
 namespace TheDonHimself\GremlinOGM\Serializer\Handler;
 
 use DateTime;
-use DateTimeZone;
 use JMS\Serializer\Context;
 use JMS\Serializer\GraphNavigator;
 use JMS\Serializer\Handler\SubscribingHandlerInterface;
@@ -11,17 +10,6 @@ use JMS\Serializer\JsonSerializationVisitor;
 
 class SerializerDateSubscriber implements SubscribingHandlerInterface
 {
-    private $defaultFormat;
-    private $defaultTimezone;
-    private $xmlCData;
-
-    public function __construct($defaultFormat = DateTime::ISO8601, $defaultTimezone = 'UTC', $xmlCData = true)
-    {
-        $this->defaultFormat = $defaultFormat;
-        $this->defaultTimezone = new DateTimeZone($defaultTimezone);
-        $this->xmlCData = $xmlCData;
-    }
-
     public static function getSubscribingMethods()
     {
         return array(
@@ -36,30 +24,7 @@ class SerializerDateSubscriber implements SubscribingHandlerInterface
 
     public function serializeDateTimeToString(JsonSerializationVisitor $visitor, DateTime $date, array $type, Context $context)
     {
-        if ($this->isInGroups(['Graph'], $context)) {
-            return $date->getTimestamp() * 1000;
-        }
-
-        return $visitor->visitString($date->format($this->getFormat($type)), $type, $context);
+        return $date->getTimestamp() * 1000;
     }
 
-    private function isInGroups(array $validGroup, Context $context)
-    {
-        $option = $context->attributes->get('groups')
-                ->filter(function ($groups) use ($validGroup) {
-                    return sizeof(array_intersect($groups, $validGroup)) > 0;
-                });
-
-        return !$option->isEmpty();
-    }
-
-    /**
-     * @param array $type
-     *
-     * @return string
-     */
-    private function getFormat(array $type)
-    {
-        return isset($type['params'][0]) ? $type['params'][0] : $this->defaultFormat;
-    }
 }
